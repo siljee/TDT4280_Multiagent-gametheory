b8fccdf5562697c5635c2a96f606082e6e06b974
Marius Fagerland <mariufa@null19.math.ntnu.no>
2015-02-10 15:43:46 +0100

    Added a register to yellow pages for AdditionSolver

--- src/Oving2/AdditionSolver.java
+++ src/Oving2/AdditionSolver.java
@@ -1,15 +1,33 @@
 package Oving2;
 
 import jade.core.Agent;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
 
 public class AdditionSolver extends Agent{
 	
 	
 	public void setup() {
+		DFAgentDescription dfd = new DFAgentDescription();
+		dfd.setName(getAID());
+		ServiceDescription sd = new ServiceDescription();
+		sd.setType("Adder");
+		sd.setName("JADE-adder");
+		dfd.addServices(sd);
+		try {
+			DFService.register(this, dfd);
+		} catch (FIPAException e) {
+			e.printStackTrace();
+		}
 		
 	}
 	
 	public void takeDown() {
+		try {
+			DFService.deregister(this);
+		}
 		
 	}
 
